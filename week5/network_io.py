7ad88d5e3b49cb6df498ceb809bdf7c83c59686a
macs1207 <kenginlim@gmail.com>
2020-04-05 02:29:10 +0800

    networkio

--- week5/network_io.py
+++ week5/network_io.py
@@ -1,12 +1,78 @@
+import socket
+import struct
+import sys
+
+
 class NetworkIO:
+    
+    sock = None
+    
     def __init__(self, sock):
-        pass
+        self.sock = sock
     
     def read(self):
-        pass
+        data = self._nbyte_to_data(self.sock)
+        if not data:
+            return None
+        return f"{type(data).__name__} {repr(data)}"
+        
+        
+    def write(self, n):
+        self.sock.send(self._data_to_nbyte(n))
     
-    def write(self):
-        pass
+    def _data_to_nbyte(self, n):
+        if isinstance(n, int):
+            if n < (1 << 8):
+                tag = b'B'
+            elif n < (1 << 16):
+                tag = b'H'
+            elif n < (1 << 32):
+                tag = b"L"
+            else:
+                tag = b'Q'
+            result = tag + struct.pack('!' + tag.decode(), n)
+            return result
+        elif isinstance(n, bytes):
+            result = b's' + self._data_to_nbyte(len(n)) + n
+            return result
+        elif isinstance(n, str):
+            n = n.encode('UTF-8')
+            result = b'c' + self._data_to_nbyte(len(n)) + n
+            return result
+        elif isinstance(n, float):
+            result = b'f' + struct.pack('!f', n)
+            return result
+        raise TypeError('Invalid type: ' + str(type(n)))
+
+
+    def _nbyte_to_data(self, sock):
+        size_info = {'B': 1, 'H': 2, 'L': 4, 'Q': 8}
+
+        btag = sock.recv(1)
+        if not btag:
+            return None
+        else:
+            tag = btag.decode('UTF-8')
+
+        if tag in size_info:
+            size = size_info[tag]
+            bnum = sock.recv(size)
+            result = struct.unpack('!' + tag, bnum)[0]
+        elif tag in "sc":
+            size = self._nbyte_to_data(sock)
+            if size >= 65536:
+                raise ValueError('length too long: ' + str(size))
+            bstr = sock.recv(size)
+            if tag == 's':
+                result = bstr
+            else:
+                result = bstr.decode('UTF-8')
+        elif tag == "f":
+            bnum = sock.recv(4)
+            result = struct.unpack("!f", bnum)[0]
+        else:
+            raise TypeError('Invalid type: ' + tag)
+        return result
     
 
 def server(host, port):
@@ -40,7 +106,18 @@ def client(host, port):
     sock.close()
 
 def main():
-    pass
+    msg = "Usage: %s {server|client} host port" % sys.argv[0]
+    if len(sys.argv) != 4:
+        print(msg)
+    else:
+        host = sys.argv[2]
+        port = int(sys.argv[3])
+        if sys.argv[1] == "server":
+            server(host, port)
+        elif sys.argv[1] == "client":
+            client(host, port)
+        else:
+            print(msg)
 
 if __name__ == "__main__":
     main()
