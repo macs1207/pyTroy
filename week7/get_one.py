ca1a927ebfe8f3ceeccc843b5610cf66bfb3908c
macs1207 <kenginlim@gmail.com>
2020-05-04 19:12:05 +0800

    Update week7 get_one solution and Add week8

--- week7/get_one.py
+++ week7/get_one.py
@@ -1,6 +1,13 @@
-cnt = 0
+def solution1(n):
+    cnt = 0
 
-for i in range(1, 200):
-    cnt += str(i).count("1")
-    
-print(cnt)
+    for i in range(1, n + 1):
+        cnt += str(i).count("1")
+    print(cnt)
+
+def solution2(n):
+    print(sum([str(i).count('1') for i in range(n + 1)]))
+
+n = 199
+solution1(n)
+solution2(n)
